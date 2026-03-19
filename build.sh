7309d73026f5431343d2514f6964be1277853f74
Guanpeng Xu <herberteuler@gmail.com>
2026-03-18 20:54:04 -0700

    Get plugins if it is absent

--- build.sh
+++ build.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
 
 if [[ $# != 2 ]]; then
-    echo Usage: $0 root-dir branch-name 1>&2
+    echo Usage: "$0" root-dir branch-name 1>&2
     exit 1
 fi
 
 REPO_URL=https://github.com/JetBrains/intellij-community.git
 ROOT_DIR=$1
 BRANCH_NAME=$2
-RELEASE=$(echo $BRANCH_NAME | awk -F '[/.]' '{ print $2 }')
-PATCHES_DIR=$(readlink -f $(dirname $0)/patches)
+RELEASE=$(echo "$BRANCH_NAME" | awk -F '[/.]' '{ print $2 }')
+PATCHES_DIR=$(readlink -f "$(dirname "$0")"/patches)
 
 set -ex
 
 if [[ ! -d $ROOT_DIR ]]; then
-    git clone $REPO_URL $ROOT_DIR
-    cd $ROOT_DIR
+    git clone "$REPO_URL" "$ROOT_DIR"
+fi
+
+ANDROID_DIR="$ROOT_DIR"/android
+if [[ ! -d "$ANDROID_DIR" ]]; then
+    cd "${ROOT_DIR}"
     ./getPlugins.sh
 fi
 
-cd $ROOT_DIR/android
+cd "$ANDROID_DIR"
 git reset --hard
 git fetch
-git checkout $BRANCH_NAME
+git checkout "$BRANCH_NAME"
 
-cd $ROOT_DIR
+cd "$ROOT_DIR"
 git reset --hard
 git fetch
-git checkout $BRANCH_NAME
+git checkout "$BRANCH_NAME"
 
-cd $ROOT_DIR
-for patch in $PATCHES_DIR/*.patch; do
+cd "$ROOT_DIR"
+for patch in "$PATCHES_DIR"/*.patch; do
     alt_patch=$patch.$RELEASE
     if [[ -e $alt_patch ]]; then
 	patch=$alt_patch
     fi
-    git apply $patch
+    git apply "$patch"
 done
 
-cd $ROOT_DIR
+cd "$ROOT_DIR"
 rm -rf out
 ./installers.cmd
